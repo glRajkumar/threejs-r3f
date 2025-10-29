5764edc0243ad4ddabd937ed95c90ba44c7c0f67
Raj kumar <glrajv369@gmail.com>
2025-10-30 04:25:11 +0530

    fix: x scroll layout

--- components/docs-layout/index.tsx
+++ components/docs-layout/index.tsx
@@ -17,7 +17,7 @@ function DocsLayout({ children, title, className, ...rest }: props) {
         {...rest}
       />
 
-      <main className={cn("max-w-full flex-1 p-6 md:p-8 prose prose-sm prose-th:py-1 prose-th:px-2 prose-td:px-2 relative", className)}>
+      <main className={cn("max-w-full min-w-0 flex-1 p-6 md:p-8 prose prose-sm prose-th:py-1 prose-th:px-2 prose-td:px-2 relative", className)}>
         <DocNavbar title={title} />
 
         {children}
